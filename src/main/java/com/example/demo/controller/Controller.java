bcf88dde577b5d34a5c263e8604af1e1dc239330
waibazen <waibazen@gmail.com>
2022-02-27 14:54:07 -0800

    first git commit files

---
+++ src/main/java/com/example/demo/controller/Controller.java
@@ -0,0 +1,83 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.Student;
+import com.example.demo.repository.StudentRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+
+public class Controller {
+
+    @Autowired
+    private StudentRepo studentRepo;
+
+
+
+    @GetMapping("/")
+    public String  home() {
+        return "<h1>welcome to the Student Portal</h1>";
+    }
+    @GetMapping("/students")
+    public List<Student> getAllStudents() {
+        return studentRepo.findAll();
+    }
+    @GetMapping("/student/{id}")
+    public Student getStudentById(@PathVariable("id") Long id) {
+
+        Optional<Student> student= studentRepo.findById(id);//method ko return type optional rakham for nullcheck
+        if (student.isEmpty())
+            throw new RuntimeException("No Cusotmer git  found");//not working
+
+        return student.get();
+    }
+
+
+
+
+    @PostMapping("/student/add")
+    public Student createStudent(@RequestBody Student student) {
+        return studentRepo.save(student);
+    }
+
+
+    @DeleteMapping("/student/{id}")
+    public String deleteStudentById(@PathVariable("id") Long id) {
+
+        Optional<Student> student = studentRepo.findById(id);
+        if (student.isEmpty())
+            throw new RuntimeException("No Cusotmer found");
+            studentRepo.delete(student.get());
+
+        return " student deleted:"+id;
+    }
+//	@PostMapping("/employee/{id}")
+//	public String updateEmployee(@PathVariable("id") Long id) {
+//
+//		Optional<Employee> employee = employeeRepository.findById(id);
+//		if (employee.isEmpty())
+//			throw new RuntimeException("No Employee found");
+//		employeeRepository.saveorUpdate(employee.get());
+//
+//		return "employee deleted";
+//	}
+
+    @PutMapping("/student/{id}")
+    public  Student updateStudent(@RequestBody Student student, @PathVariable long id) {
+
+        Optional<Student> studentOptional = studentRepo.findById(id);
+
+        if (studentOptional.isEmpty())
+            throw new RuntimeException("No Employee found");
+
+
+        return studentRepo.save(student);
+
+
+    }
+
+
+}
---
+++ src/main/java/com/example/demo/model/Student.java
@@ -0,0 +1,53 @@
+package com.example.demo.model;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+
+
+@Table(name="students")
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private long id;
+    private String firstName;
+    private String LastName;
+    private String email;
+    private String Address;
+    public long getId() {
+        return id;
+    }
+    public void setId(long id) {
+        this.id = id;
+    }
+    public String getFirstName() {
+        return firstName;
+    }
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return LastName;
+    }
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getAddress() {
+        return Address;
+    }
+    public void setAddress(String address) {
+        Address = address;
+    }
+}
+
